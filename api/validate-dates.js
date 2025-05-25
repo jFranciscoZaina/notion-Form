9bc9ad806e9dcb5380dea38827e17d31d219567c
jFranciscoZaina <jfranciscozaina@gmail.com>
2025-05-25 18:44:50 +0200

    CORS para vercel

--- api/validate-dates.js
+++ api/validate-dates.js
@@ -1,3 +1,12 @@
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS")
+res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+
+if (req.method === "OPTIONS") {
+  res.status(200).end()
+  return
+}
+
 const { Client } = require("@notionhq/client")
 
 const notion = new Client({
